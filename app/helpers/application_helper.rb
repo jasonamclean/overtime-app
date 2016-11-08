2f2b2a17905ba3cfc1928a8d054552ab43fefd96
Jason McLean <jasonamclean@hotmail.com>
2016-11-07 22:14:19 -0700

    Added admin dashboard page styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
+  def admin_types
+    ['AdminUser']
+  end
+
   def active?(path)
     "active" if current_page?(path)
   end
 
   def status_label status
     status_span_generator status
-  end
-
-  def admin_types
-    ['AdminUser']
   end
 
   private
